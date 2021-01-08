abfd2701f862883e220f5875df4fd281196da5e3
RogerMKobus <roger.kobus@gmail.com>
2021-01-08 19:34:38 -0300

    Ending with the use of the State Hook

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function MainPage() {
+  const [ randomNumber, setRandomNumber ] = useState(0);
+
+  function generateNumber(){
+    const newNumber = Math.floor(Math.random() * (100-1) +1);
+    setRandomNumber(newNumber);
+  }
+
   return(
     <div className="center">
         <h3>Random Number:</h3>
-        <h1>0</h1>
+        <h1>{ randomNumber }</h1>
         
         <div className='button-area'>
           <label>
             Click in the button to genarate a random number:
           </label>
           
-          <button>
+          <button onClick={ generateNumber }>
              Genarate Number
           </button>
         </div>
